ef4691ae9549a71db9b1b8510482526367672bd5
Rafael Dalzotto <rafaeldalzotto.contacto@gmail.com>
2020-07-30 08:35:04 -0300

    FrontEnd renovado!

--- lib/frontend/screens/home_page/home_page.dart
+++ lib/frontend/screens/home_page/home_page.dart
@@ -15,7 +15,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  int menuId = 2;
+  int _menuIndex = 2;
+  PageController _pageController;
+    @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 2);
+  }
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,47 +37,48 @@ class _HomePageState extends State<HomePage> {
         iconTheme: IconThemeData(color: Colors.grey),
         // leading: MenuButton(),
       ),
-      body: ModulosPage(),
-      backgroundColor: Color.fromRGBO(245, 245, 245, 1),
+      // body: ModulosPage(),
+      body: PageView(
+        scrollDirection: Axis.vertical,
+        physics: NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        /* onPageChanged: (index) {
+          setState(() => _menuIndex = index);
+        }, */
+        children: <Widget>[
+          Container(),
+          Container(),
+          ModulosPage(),
+          Container(),
+          Container(),
+          Container(),
+          Container(),
+          Container(),
+        ],
+      ),
+      backgroundColor: Color.fromRGBO(235, 235, 235, 1),
       drawerEdgeDragWidth: 35,
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
             // DrawerHeader(child: , margin: EdgeInsets.zero,),
             Container(
-              color: Colors.grey.withOpacity(0.2),
+              color: Colors.white, //.withOpacity(0.2),
               child: Padding(
                 padding: const EdgeInsets.only(top: 18.0, left: 18, bottom: 14),
                 child: AppBarTitle(title: 'App',),
               ),
             ),
             // Divider(thickness: 1.5,),
-            CustomMenuTile(0, 'Tu perfil', Icons.person, menuId, (){
-
-            }),
-            CustomMenuTile(1 ,'Amigos', Icons.group, menuId, (){
-
-            }),
+            CustomMenuTile(0, 'Tu perfil', Icons.person, _pageController),
+            CustomMenuTile(1 ,'Amigos', Icons.group, _pageController),
             Divider(),
-            CustomMenuTile(2 ,'Dispositivos', Icons.view_list, menuId, (){
-
-            }),
-            CustomMenuTile(3 ,'Automatización', Icons.functions, menuId, (){
-
-            }),
-            CustomMenuTile(4 ,'Configuración', Icons.settings, menuId, (){
-
-            }),
-            CustomMenuTile(5 ,'Seguridad', Icons.security, menuId, (){
-
-            }),
+            CustomMenuTile(2 ,'Dispositivos', Icons.view_list, _pageController),
+            CustomMenuTile(3 ,'Automatización', Icons.functions, _pageController),
+            CustomMenuTile(4 ,'Configuración', Icons.settings, _pageController),
             Divider(),
-            CustomMenuTile(6 ,'Información', Icons.info_outline, menuId, (){
-
-            }),
-            CustomMenuTile(7 ,'Ayuda', Icons.help_outline, menuId, (){
-
-            }),
+            CustomMenuTile(6 ,'Información', Icons.info_outline, _pageController),
+            CustomMenuTile(7 ,'Ayuda', Icons.help_outline, _pageController),
           ],
         ),
       ),
@@ -78,21 +90,24 @@ class CustomMenuTile extends StatelessWidget {
 
   String title;
   IconData icon;
-  Function onTap;
   bool selected;
-  int id, currentId;
+  int id;
+  PageController controller;
 
-  CustomMenuTile(this.id, this.title, this.icon, this.currentId, this.onTap);
+  CustomMenuTile(this.id, this.title, this.icon, this.controller);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       splashColor: Colors.lightBlueAccent,
-      onTap: onTap,
+      onTap: (){
+        controller.jumpToPage(id);
+        Navigator.of(context).pop();
+      },
       child: ListTile(
         title: Text(title),
         leading: Icon(icon),
-        selected: id == currentId,
+        selected: id == controller.page,
       ),
     );
   }
